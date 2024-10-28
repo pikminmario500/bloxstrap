a1d43aad7cb55911f68ea2b2f71cf8d1f6cd455a
pikminfan3009 <88578103+pikminmario500@users.noreply.github.com>
2024-10-28 19:22:03 +0100

    add a scam warning to the flag editor

---
+++ Bloxstrap/UI/Elements/Settings/Pages/FastFlagEditorScamWarningPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace Bloxstrap.UI.Elements.Settings.Pages
+{
+    /// <summary>
+    /// Interaction logic for FastFlagEditorScamWarningPage.xaml
+    /// </summary>
+    public partial class FastFlagEditorScamWarningPage
+    {
+        public FastFlagEditorScamWarningPage() : base(typeof(FastFlagEditorPage))
+        {
+            InitializeComponent();
+        }
+
+        protected override void ContinueCallback()
+        {
+            App.State.Prop.ShowFFlagEditorWarnings = false;
+            App.State.Save(); // should we be force saving?
+        }
+    }
+}
