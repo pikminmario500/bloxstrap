5ab9d8cf35895f348f9a459295b37e854cbf3c2a
pikminmario500 <88578103+pikminmario500@users.noreply.github.com>
2025-03-23 19:34:55 +0100

    merge 2

--- Bloxstrap/UI/ViewModels/Editor/BootstrapperEditorWindowViewmodel.cs
+++ Bloxstrap/UI/ViewModels/Editor/BootstrapperEditorWindowViewmodel.cs
@@ -1,5 +1,10 @@
-using Bloxstrap.UI.Elements.Bootstrapper;
+﻿using Bloxstrap.UI.Elements.Bootstrapper;
 using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -11,10 +16,17 @@ namespace Bloxstrap.UI.ViewModels.Editor
 
         public ICommand PreviewCommand => new RelayCommand(Preview);
         public ICommand SaveCommand => new RelayCommand(Save);
+        public ICommand OpenThemeFolderCommand => new RelayCommand(OpenThemeFolder);
+
+        public Action<bool, string> ThemeSavedCallback { get; set; } = null!;
+
+        public string Directory { get; set; } = "";
 
         public string Name { get; set; } = "";
         public string Title { get; set; } = "Editing \"Custom Theme\"";
         public string Code { get; set; } = "";
+
+        public bool CodeChanged { get; set; } = false;
 
         private void Preview()
         {
@@ -26,8 +38,7 @@ namespace Bloxstrap.UI.ViewModels.Editor
 
                 dialog.ApplyCustomTheme(Name, Code);
 
-                if (_dialog != null)
-                    _dialog.CloseBootstrapper();
+                _dialog?.CloseBootstrapper();
                 _dialog = dialog;
 
                 dialog.Message = Strings.Bootstrapper_StylePreview_TextCancel;
@@ -39,7 +50,7 @@ namespace Bloxstrap.UI.ViewModels.Editor
                 App.Logger.WriteLine(LOG_IDENT, "Failed to preview custom theme");
                 App.Logger.WriteException(LOG_IDENT, ex);
 
-                Frontend.ShowMessageBox($"Failed to preview theme: {ex.Message}", MessageBoxImage.Error, MessageBoxButton.OK);
+                Frontend.ShowMessageBox(string.Format(Strings.CustomTheme_Editor_Errors_PreviewFailed, ex.Message), MessageBoxImage.Error, MessageBoxButton.OK);
             }
         }
 
@@ -47,19 +58,27 @@ namespace Bloxstrap.UI.ViewModels.Editor
         {
             const string LOG_IDENT = "BootstrapperEditorWindowViewModel::Save";
 
-            string path = Path.Combine(Paths.CustomThemes, Name, "Theme.xml");
+            string path = Path.Combine(Directory, "Theme.xml");
 
             try
             {
                 File.WriteAllText(path, Code);
+                CodeChanged = false;
+                ThemeSavedCallback.Invoke(true, Strings.CustomTheme_Editor_Save_Success_Description);
             }
             catch (Exception ex)
             {
                 App.Logger.WriteLine(LOG_IDENT, "Failed to save custom theme");
                 App.Logger.WriteException(LOG_IDENT, ex);
 
-                Frontend.ShowMessageBox($"Failed to save theme: {ex.Message}", MessageBoxImage.Error, MessageBoxButton.OK);
+                //Frontend.ShowMessageBox($"Failed to save theme: {ex.Message}", MessageBoxImage.Error, MessageBoxButton.OK);
+                ThemeSavedCallback.Invoke(false, ex.Message);
             }
+        }
+
+        private void OpenThemeFolder()
+        {
+            Process.Start("explorer.exe", Directory);
         }
     }
 }
