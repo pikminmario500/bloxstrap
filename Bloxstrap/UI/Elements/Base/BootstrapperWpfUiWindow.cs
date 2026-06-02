f23148b2f670964b74edefd121840270c4ed4292
otsako <88578103+otsako-vpk@users.noreply.github.com>
2026-06-02 19:24:30 +0200

    seperate bootstrappers from global theme

---
+++ Bloxstrap/UI/Elements/Base/BootstrapperWpfUiWindow.cs
@@ -0,0 +1,36 @@
+using System.Windows;
+using System.Windows.Interop;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Services;
+
+namespace Bloxstrap.UI.Elements.Base
+{
+    public abstract class BootstrapperWpfUiWindow : WpfUiWindow
+    {
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            if (App.Settings.Prop.WPFSoftwareRender || App.LaunchSettings.NoGPUFlag.Active)
+            {
+                if (PresentationSource.FromVisual(this) is HwndSource hwndSource)
+                    hwndSource.CompositionTarget.RenderMode = RenderMode.SoftwareOnly;
+            }
+
+            base.OnSourceInitialized(e);
+
+            ThemeType type = App.Settings.Prop.BootstrapperTheme.GetFinal() == Enums.Theme.Dark ? ThemeType.Dark : ThemeType.Light;
+
+            if (type == ThemeType.Dark)
+                Wpf.Ui.Appearance.Theme.ApplyDarkThemeToWindow(this);
+            else
+                Wpf.Ui.Appearance.Theme.RemoveDarkThemeFromWindow(this);
+
+            string name = type == ThemeType.Dark ? "Dark" : "Light";
+            this.Resources.MergedDictionaries.Insert(0, new ResourceDictionary
+            {
+                Source = new Uri($"pack://application:,,,/Wpf.Ui;component/Styles/Theme/{name}.xaml", UriKind.Absolute)
+            });
+        }
+    }
+}
